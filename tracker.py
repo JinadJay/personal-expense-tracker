fbb4f065d08fffcacd5ef5abd7d19ff87a6333b2
unknown <jinadjeremiah@gmail.com>
2025-12-31 13:04:31 +0100

    Updated the code with new features

--- tracker.py
+++ tracker.py
@@ -1,3 +1,16 @@
-amount = input("Amount Spent: ")
-category = input("Category: ")
-print(f"You spent ${amount} on {category}")
+expenses = []
+use = []
+
+while True:
+    amount = int(input("Enter amount: "))
+    category = input("Enter category: ")
+
+    expenses.append(amount)
+    use.append(category)
+
+    choice = input("Press 1 to add, 2 to exit: ")
+    if choice == "2":
+        break
+
+for i in range(len(expenses)):
+    print(f"amount: {expenses[i]} | Category: {use[i]}")
