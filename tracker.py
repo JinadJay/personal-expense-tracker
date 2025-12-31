cd2de62e50d95364e7eba836938a926f90bd8b2f
unknown <jinadjeremiah@gmail.com>
2025-12-31 13:22:48 +0100

    Updated the code with date feature

--- tracker.py
+++ tracker.py
@@ -1,16 +1,19 @@
 expenses = []
 use = []
+date = []
 
 while True:
     amount = int(input("Enter amount: "))
     category = input("Enter category: ")
+    time = input("Input Date: ")
 
     expenses.append(amount)
     use.append(category)
+    date.append(time)
 
     choice = input("Press 1 to add, 2 to exit: ")
     if choice == "2":
         break
 
 for i in range(len(expenses)):
-    print(f"amount: {expenses[i]} | Category: {use[i]}")
+    print(f"amount: {expenses[i]} | category: {use[i]} | time: {date[i]}")
